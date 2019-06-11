eb51d974e6b9104cbec871b266cd45aeefee2be1
Daniel Schneider <daschne8@gmail.com>
2019-06-11 16:01:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name){
   drivers.push(name)
 }
- function destructivelyPrependDriver(name){
-   drivers.unshift(name)
- }
+function destructivelyPrependDriver(name){
+ drivers.unshift(name)
+}
 function destructivelyRemoveLastDriver(){
-
+  drivers.pop()
 }
